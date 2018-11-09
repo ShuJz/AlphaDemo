e8b361972e9c8a8dbf268de3a76403b89790af29
jingzhe shu <shujingzhe1996@gmail.com>
2018-11-09 20:58:50 +0800

    add moveit! control demo

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
+subdirs(arm_planning)
 subdirs(alpha_description)
+subdirs(alpha_moveit_config)
---
+++ build/alpha_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jingzhe/WorkSpace/ROS/AlphaDemo/src/alpha_moveit_config
+# Build directory: /home/jingzhe/WorkSpace/ROS/AlphaDemo/build/alpha_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
